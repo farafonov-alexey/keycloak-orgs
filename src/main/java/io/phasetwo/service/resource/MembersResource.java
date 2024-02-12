5bb16487808e52ea61d9ce5ecba8c1fa4ac4d295
reavolt <reavolt@outlook.com>
2024-02-12 15:46:13 +0300

    Вернул UserRepresentationWithRoles

--- src/main/java/io/phasetwo/service/resource/MembersResource.java
+++ src/main/java/io/phasetwo/service/resource/MembersResource.java
@@ -2,13 +2,21 @@ package io.phasetwo.service.resource;
 
 import static io.phasetwo.service.resource.OrganizationResourceType.*;
 import static org.keycloak.models.utils.ModelToRepresentation.*;
+import static io.phasetwo.service.resource.Converters.*;
 
 import com.google.common.base.Strings;
 import io.phasetwo.service.model.OrganizationModel;
+import io.phasetwo.service.model.OrganizationRoleModel;
+import io.phasetwo.service.representation.BulkResponseItem;
+import io.phasetwo.service.representation.OrganizationRole;
+import io.phasetwo.service.representation.UserRepresentationWithRoles;
 import jakarta.validation.constraints.*;
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.stream.Stream;
 import lombok.extern.jbosslog.JBossLog;
 import org.keycloak.events.admin.OperationType;
@@ -21,7 +29,8 @@ public class MembersResource extends OrganizationAdminResource {
 
   private final OrganizationModel organization;
 
-  public MembersResource(OrganizationAdminResource parent, OrganizationModel organization) {
+  public MembersResource(OrganizationAdminResource parent,
+                         OrganizationModel organization) {
     super(parent);
     this.organization = organization;
   }
@@ -29,13 +38,15 @@ public class MembersResource extends OrganizationAdminResource {
   @GET
   @Path("")
   @Produces(MediaType.APPLICATION_JSON)
-  public Stream<UserRepresentation> getMembers(
-      @QueryParam("search") String searchQuery,
-      @QueryParam("first") Integer firstResult,
-      @QueryParam("max") Integer maxResults) {
-    log.debugf("Get members for %s %s [%s]", realm.getName(), organization.getId(), searchQuery);
+  public Stream<UserRepresentation>
+  getMembers(@QueryParam("search") String searchQuery,
+             @QueryParam("first") Integer firstResult,
+             @QueryParam("max") Integer maxResults) {
+    log.debugf("Get members for %s %s [%s]", realm.getName(),
+               organization.getId(), searchQuery);
     firstResult = firstResult != null ? firstResult : 0;
-    maxResults = maxResults != null ? maxResults : Constants.DEFAULT_MAX_RESULTS;
+    maxResults =
+        maxResults != null ? maxResults : Constants.DEFAULT_MAX_RESULTS;
     return organization
         .searchForMembersStream(searchQuery, firstResult, maxResults)
         .map(m -> toRepresentation(session, realm, m));
@@ -45,7 +56,8 @@ public class MembersResource extends OrganizationAdminResource {
   @Path("count")
   @Produces(MediaType.APPLICATION_JSON)
   public Long getMembersCount() {
-    log.debugf("Get members count for %s %s", realm.getName(), organization.getId());
+    log.debugf("Get members count for %s %s", realm.getName(),
+               organization.getId());
     return organization.getMembersCount();
   }
 
@@ -54,18 +66,18 @@ public class MembersResource extends OrganizationAdminResource {
   public Response removeMember(@PathParam("userId") String userId) {
     canDelete(userId);
 
-    log.debugf("Remove member %s from %s %s", userId, realm.getName(), organization.getId());
+    log.debugf("Remove member %s from %s %s", userId, realm.getName(),
+               organization.getId());
     UserModel member = session.users().getUserById(realm, userId);
-    if (!Strings.isNullOrEmpty(member.getUsername())
-        && member
-            .getUsername()
-            .equals(OrganizationResourceProviderFactory.getDefaultAdminUsername(organization))) {
+    if (!Strings.isNullOrEmpty(member.getUsername()) &&
+        member.getUsername().equals(
+            OrganizationResourceProviderFactory.getDefaultAdminUsername(
+                organization))) {
       throw new ForbiddenException("Cannot remove default organization user.");
     }
     if (member != null && organization.hasMembership(member)) {
       organization.revokeMembership(member);
-      adminEvent
-          .resource(ORGANIZATION_MEMBERSHIP.name())
+      adminEvent.resource(ORGANIZATION_MEMBERSHIP.name())
           .operation(OperationType.DELETE)
           .resourcePath(session.getContext().getUri())
           .representation(userId)
@@ -79,7 +91,8 @@ public class MembersResource extends OrganizationAdminResource {
   @GET
   @Path("{userId}")
   public Response getMember(@PathParam("userId") String userId) {
-    log.debugf("Check membership %s for %s %s", userId, realm.getName(), organization.getId());
+    log.debugf("Check membership %s for %s %s", userId, realm.getName(),
+               organization.getId());
     UserModel member = session.users().getUserById(realm, userId);
     if (member != null && organization.hasMembership(member)) {
       return Response.noContent().build();
@@ -92,33 +105,60 @@ public class MembersResource extends OrganizationAdminResource {
   @Path("{userId}")
   @Produces(MediaType.APPLICATION_JSON)
   public Response addMember(@PathParam("userId") String userId) {
-    log.debugf("Add %s as member for %s %s", userId, realm.getName(), organization.getId());
+    log.debugf("Add %s as member for %s %s", userId, realm.getName(),
+               organization.getId());
     canManage();
 
     UserModel member = session.users().getUserById(realm, userId);
     if (member != null) {
       if (!organization.hasMembership(member)) {
         organization.grantMembership(member);
-        adminEvent
-            .resource(ORGANIZATION_MEMBERSHIP.name())
+        adminEvent.resource(ORGANIZATION_MEMBERSHIP.name())
             .operation(OperationType.CREATE)
             .resourcePath(session.getContext().getUri())
             .representation(userId)
             .success();
       }
-      return Response.created(session.getContext().getUri().getAbsolutePathBuilder().build())
+      return Response
+          .created(
+              session.getContext().getUri().getAbsolutePathBuilder().build())
           .build();
     } else {
       throw new NotFoundException();
     }
   }
 
+  @GET
+  @Path("/with-roles")
+  @Produces(MediaType.APPLICATION_JSON)
+  public Stream<UserRepresentationWithRoles>
+  getMembersWithRoles(@QueryParam("search") String searchQuery,
+                      @QueryParam("first") Integer firstResult,
+                      @QueryParam("max") Integer maxResults) {
+    log.debugf("Get members for %s %s [%s]", realm.getName(),
+               organization.getId(), searchQuery);
+    firstResult = firstResult != null ? firstResult : 0;
+    maxResults =
+        maxResults != null ? maxResults : Constants.DEFAULT_MAX_RESULTS;
+    return organization
+        .searchForMembersStream(searchQuery, firstResult, maxResults)
+        .map(m -> {
+          UserRepresentation user = toRepresentation(session, realm, m);
+          List<OrganizationRole> roles =
+              organization.getRolesStream()
+                  .filter(r -> r.hasRole(m))
+                  .map(r -> convertOrganizationRole(r))
+                  .toList();
+          return new UserRepresentationWithRoles().user(user).organizationRoles(
+              roles);
+        });
+  }
+
   private void canManage() {
     if (!auth.hasManageOrgs() && !auth.hasOrgManageMembers(organization)) {
-      throw new NotAuthorizedException(
-          String.format(
-              "User %s doesn't have permission to manage members in org %s",
-              auth.getUser().getId(), organization.getName()));
+      throw new NotAuthorizedException(String.format(
+          "User %s doesn't have permission to manage members in org %s",
+          auth.getUser().getId(), organization.getName()));
     }
   }
 
